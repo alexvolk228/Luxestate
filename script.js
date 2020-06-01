9a49a7253b817e4a63e12fae1814f5f605280f61
alexvolk228 <aleksey_volk@mail.ru>
2020-06-01 18:45:51 +0300

    Finished Site
    Optimized fo iPhone X

---
+++ script.js
@@ -0,0 +1,32 @@
+let stepbl1 = document.getElementById('stepbl1');
+let stepbrdr1 = document.getElementById('stepbrdr1');
+
+stepbl1.onmouseover = function () {
+    stepbrdr1.style.borderBottom = "3px solid white";
+}
+
+stepbl1.onmouseout = function () {
+    stepbrdr1.style.borderBottom = "3px solid #1f373d";
+}
+
+let stepbl2 = document.getElementById('stepbl2');
+let stepbrdr2 = document.getElementById('stepbrdr2');
+
+stepbl2.onmouseover = function () {
+    stepbrdr2.style.borderBottom = "3px solid white";
+}
+
+stepbl2.onmouseout = function () {
+    stepbrdr2.style.borderBottom = "3px solid #1f373d";
+}
+
+let stepbl3 = document.getElementById('stepbl3');
+let stepbrdr3 = document.getElementById('stepbrdr3');
+
+stepbl3.onmouseover = function () {
+    stepbrdr3.style.borderBottom = "3px solid white";
+}
+
+stepbl3.onmouseout = function () {
+    stepbrdr3.style.borderBottom = "3px solid #1f373d";
+}
